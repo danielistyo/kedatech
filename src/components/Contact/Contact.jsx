b3e1cbe2f6a9a36fd06085342e3a93f2d9915ace
Daniel <danielistyo16@gmail.com>
2023-08-20 20:22:17 +0700

    add contact component

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { Col, Container, Form, Row } from "react-bootstrap";
+import "./Contact.scss";
+
+const Contact = () => {
+  return (
+    <div id="contact" className="contact-section">
+      <Container>
+        <Row>
+          <Col>
+            <div className="contact-detail">
+              <h1 className="title">Contact Us</h1>
+              <p className="description">
+                Write down your message in this form below. We will call you as soon as possible.
+              </p>
+            </div>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Form>
+              <Row>
+                <Col md={6}>
+                  <Form.Group>
+                    <Form.Label>Name</Form.Label>
+                    <Form.Control type="text" placeholder="Name" />
+                  </Form.Group>
+                </Col>
+                <Col md={6}>
+                  <Form.Group>
+                    <Form.Label>Phone</Form.Label>
+                    <Form.Control type="number" placeholder="Phone" />
+                  </Form.Group>
+                </Col>
+              </Row>
+              <Col>
+                <Form.Group>
+                  <Form.Label>Message</Form.Label>
+                  <Form.Control as="textarea" rows={4} />
+                </Form.Group>
+              </Col>
+            </Form>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+};
+
+export default Contact;
