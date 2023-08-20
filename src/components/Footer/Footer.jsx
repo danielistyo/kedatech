4e2dab312da8a2b4052b458a8619ab2e9ab5e8a8
Daniel <danielistyo16@gmail.com>
2023-08-20 20:22:49 +0700

    add footer component

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Footer.scss";
+
+const Footer = () => {
+  return <footer className="footer-section">by <b>ERP SERBA BISA</b></footer>;
+};
+
+export default Footer;
